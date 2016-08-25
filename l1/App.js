5d243fb07334029a1c6b17c1dd08a6322e123827
Yaowenjie <wjyao@thoughtworks.com>
2016-08-25 23:11:28 +0800

    Add webpack-dashboard

--- l1/App.js
+++ l1/App.js
@@ -38,7 +38,6 @@ class App extends React.Component {
       </div>
     );
   };
-
 }
 
 export default App;
--- l1/webpack.config.js
+++ l1/webpack.config.js
@@ -1,15 +1,40 @@
+var Dashboard = require('webpack-dashboard');
+var DashboardPlugin = require('webpack-dashboard/plugin');
+
+var dashboard = new Dashboard();
+
 module.exports = {
   entry: {
     app: ['./main.js', './counter.js']
   },
+  quiet: false,
   output: {
     path : './',
     filename: 'index.js'
   },
   devServer: {
+    stats: {
+      colors: true,
+      hash: false,
+      version: false,
+      timings: false,
+      assets: false,
+      chunks: false,
+      modules: false,
+      reasons: false,
+      children: false,
+      source: false,
+      errors: false,
+      errorDetails: false,
+      warnings: false,
+      publicPath: false
+    },
     inline: true,
     port: 3333
   },
+  plugins: [
+    new DashboardPlugin(dashboard.setData)
+  ],
   module: {
     loaders: [
       {
